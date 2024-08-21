a1b17936f8e8f60b096900311793b794cd84a1ef
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-08-21 19:26:42 +0000

    move source to src

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,11 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-export const packagePath = path.dirname(fileURLToPath(import.meta.url));
+const packagePath = path
+	.dirname(fileURLToPath(import.meta.url))
+	.split(path.sep)
+	.slice(0, -1)
+	.join(path.sep);
 
 export const CDP_FILES_PATH = path.join(packagePath, "cdp");
 export const CLASS_MAP_FILE = "class_map.json";
