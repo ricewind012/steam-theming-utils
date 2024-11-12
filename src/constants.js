0ab9f8c636c78d6bf783ce4d11c82d313d8bae48
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-11-12 19:27:07 +0000

    add config

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,16 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const packagePath = path
-	.dirname(fileURLToPath(import.meta.url))
-	.split(path.sep)
-	.slice(0, -1)
-	.join(path.sep);
+import { packagePath } from "./shared.js";
 
 export const CDP_FILES_PATH = path.join(packagePath, "cdp");
 export const SCRIPT_PATH = path.join(packagePath, "lib");
+
+export const DEFAULT_CONFIG = {
+	paths: {
+		classMaps: "class_maps",
+		dist: "dist",
+		src: "src",
+	},
+};
 
 export const PAGES = {
 	profileedit: "SteamIDEditPage",
--- src/shared.js
+++ src/shared.js
@@ -1,9 +1,19 @@
 import cdp from "chrome-remote-interface";
 import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { lilconfig } from "lilconfig";
 import { runWithResult } from "./api.js";
-import { PAGES } from "./constants.js";
+import { DEFAULT_CONFIG, PAGES } from "./constants.js";
 
-export const buildMapFileName = (page) => `class_map_${page}.json`;
+export const packagePath = path
+	.dirname(fileURLToPath(import.meta.url))
+	.split(path.sep)
+	.slice(0, -1)
+	.join(path.sep);
+
+export const getConfig = async () =>
+	(await lilconfig("steam-theming-utils").search())?.config || DEFAULT_CONFIG;
 export const readFile = (file) => fs.readFileSync(file).toString();
 
 export const selectorReplacerPlugin = (opts) => (css) => {
